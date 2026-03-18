f1e2973a35f164d78aeaa5ac42dc825463659177
Daniel Rudolf <github.com@daniel-rudolf.de>
2026-03-18 17:07:39 +0100

    Update image annotations

--- build.sh
+++ build.sh
@@ -44,6 +44,8 @@ cmd buildah run "$CONTAINER" -- \
 
 cleanup "$CONTAINER"
 
+con_cleanup "$CONTAINER"
+
 cmd buildah config \
     --annotation org.opencontainers.image.title="Ubuntu" \
     --annotation org.opencontainers.image.description="@SGSGermany's base image for containers based on Ubuntu." \
@@ -52,6 +54,7 @@ cmd buildah config \
     --annotation org.opencontainers.image.vendor="SGS Serious Gaming & Simulations GmbH" \
     --annotation org.opencontainers.image.base.name="$BASE_IMAGE" \
     --annotation org.opencontainers.image.base.digest="$(podman image inspect --format '{{.Digest}}' "$BASE_IMAGE")" \
+    --annotation org.opencontainers.image.created="$(date -u +'%+4Y-%m-%dT%H:%M:%SZ')" \
     "$CONTAINER"
 
 con_commit "$CONTAINER" "$IMAGE" "${TAGS[@]}"
